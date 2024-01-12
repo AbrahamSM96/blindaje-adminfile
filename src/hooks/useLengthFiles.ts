86fe760a599dc66658d9f76bfcb566252255a197
Abraham Serrano <abrahamserrano96@hotmail.com>
2024-01-12 14:24:12 -0600

    hook in work

---
+++ src/hooks/useLengthFiles.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'preact/hooks'
+import * as fs from 'fs'
+
+export const useLegthFiles = () => {
+  const [lengthFiles, setLegthFiles] = useState(0)
+  const dir = './public/blindaje_material/infografias'
+
+  useEffect(() => {
+    // fs.readdir(dir, (err, files) => {
+    //   console.log(files, 'HOOALALLALA')
+    //   setLegthFiles(files.length)
+    // })
+  }, [])
+
+  return { lengthFiles }
+}
