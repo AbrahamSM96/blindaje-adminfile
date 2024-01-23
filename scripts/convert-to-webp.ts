1c7480a8e5aba3cdb4e7ef5a99086f60be8d0c66
Abraham Serrano <abrahamserrano96@hotmail.com>
2024-01-22 20:21:50 -0600

    works scripts, change bun to nodejs

--- scripts/convert-to-webp.ts
+++ scripts/convert-to-webp.ts
@@ -1,34 +1,52 @@
-import { Glob } from 'bun'
 import sharp from 'sharp'
 import { join, dirname, extname, basename } from 'node:path'
-import { rm } from 'node:fs/promises'
+import { readdir, unlink, rm, rename } from 'node:fs/promises'
 
-const glob = new Glob('public/**/*.{jpg,jpeg,png}')
+const sourcePath = 'public/blindaje_material/evidencias'
+const fileExtensions = ['jpg', 'jpeg', 'png', 'webp']
 
-const replaceExtWithDot = (
-  newExtWithDot: string,
-  { inFilePath }: { inFilePath: string }
-) =>
+const replaceExtWithDot = (newExtWithDot: string, inFilePath: string) =>
   join(dirname(inFilePath), basename(inFilePath, extname(inFilePath))) +
   newExtWithDot
-const shouldRemove = (str: string = '') => str.toLowerCase().startsWith('rm')
+
+const shouldRemove = (str = '') => str.toLowerCase().startsWith('rm')
 const remove = shouldRemove(process.argv[2]?.toLowerCase())
 
-for await (const file of glob.scan('.')) {
-  console.info(`Converting ${file}`)
-  const newFilePath = replaceExtWithDot('.webp', { inFilePath: file })
-  const convert = sharp(file)
-    // .trim({ threshold: 0 }) // This removes transparent pixels
-    .webp({
+const getFiles = async (dir: string) => {
+  const dirents = await readdir(dir, { withFileTypes: true })
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = join(dir, dirent.name)
+      return dirent.isDirectory() ? getFiles(res) : res
+    })
+  )
+  return Array.prototype.concat(...files)
+}
+
+const convertAndRemoveFiles = async () => {
+  const files = await getFiles(sourcePath)
+
+  for (const file of files) {
+    if (!fileExtensions.includes(extname(file).slice(1).toLowerCase())) {
+      continue // Skip files with extensions other than jpg, jpeg, or png
+    }
+
+    console.info(`Converting ${file}`)
+
+    const newFilePath = replaceExtWithDot('.webp', file)
+
+    const convert = sharp(file).webp({
       lossless: true,
-      quality: 100
+      quality: 1
     })
 
-  await convert.toFile(newFilePath)
-  console.info(`Converted to ${newFilePath}`)
+    await convert.toFile(newFilePath)
+    console.info(`Converted to ${newFilePath}`)
 
-  if (remove) {
-    console.log(`Removing old file ${file}`)
-    await rm(file, { force: true })
+    // Delete the original file after renaming
+    await unlink(file)
+    console.info(`Deleted original file ${file}`)
   }
 }
+
+convertAndRemoveFiles()
--- scripts/create-meta-from-gallery.ts
+++ scripts/create-meta-from-gallery.ts
@@ -1,26 +1,55 @@
-import { Glob } from 'bun'
-import sharp from 'sharp'
+import { readdir } from 'node:fs/promises'
+import { promises as fs } from 'fs'
+import { join, extname } from 'path'
 import { imageMeta } from 'image-meta'
-import { join, dirname, basename } from 'node:path'
 
-const glob = new Glob(
-  'public/blindaje_material/**/infografia/*.{webp,jpg,jpeg}'
-)
+const fileNames = [
+  'documentos',
+  'evidencias',
+  'infografias',
+  'multimedia',
+  'presentaciones'
+]
 
-const metaEditions: [
-  { height: number; width: number }[],
-  { height: number; width: number }[]
-] = [[], []]
+const directory = join('public', 'blindaje_material', fileNames[1])
+const files = await fs.readdir(directory)
 
-for await (const file of glob.scan('.')) {
-  const data = await Bun.file(file).arrayBuffer()
-  const { height = 0, width = 0 } = await imageMeta(Buffer.from(data))
+const fileCount = files.length
 
-  const imageNumber = Number(file.match(/img-(\d+)/)?.[1] || '')
-  const edition = Number(file.match(/\/(\d+)\//)?.[1] || '')
+let metaEditions = Array.from({ length: fileCount - 1 }, () => ({
+  height: null,
+  width: null
+}))
 
-  metaEditions[edition - 1][imageNumber - 1] = { height, width }
+async function processGalleryImages() {
+  try {
+    const files = await readdir(directory, { withFileTypes: true })
+
+    for (const file of files) {
+      if (file.isFile() && extname(file.name) === '.webp') {
+        const filePath = join(directory, file.name)
+        const data = await fs.readFile(filePath)
+        const { height = 0, width = 0 } = await imageMeta(data)
+        // change infografias to new images
+        const edition = Number(file.name.match(/evidencias-(\d+)/)?.[1] || 0)
+
+        if (edition > 0 && edition <= metaEditions.length) {
+          metaEditions[edition - 1] = { height, width }
+        }
+      }
+    }
+
+    const outputPath = join(
+      process.cwd(),
+      'src',
+      'data',
+      'evidencias-meta-gallery.json'
+    )
+    await fs.writeFile(outputPath, JSON.stringify(metaEditions))
+    console.log(`Successfully wrote meta data to "${outputPath}"`)
+  } catch (error) {
+    console.error(`Error processing gallery images:`, error)
+  }
 }
 
-const outputPath = join(process.cwd(), 'src/data/meta-gallery.json')
-await Bun.write(outputPath, JSON.stringify(metaEditions))
+processGalleryImages()
