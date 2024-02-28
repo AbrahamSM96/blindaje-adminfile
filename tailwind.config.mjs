9e6e78567ab553bc2d07fdc05d3740d92edacbe6
Abraham Serrano <abrahamserrano96@hotmail.com>
2024-02-27 22:06:37 -0600

    add dark theme, still fix issue when change route

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)'
+      },
       animation: {
         flip: 'flip 6s infinite steps(2, end)',
         rotate: 'rotate 3s linear infinite both'
@@ -21,5 +27,11 @@ export default {
       }
     }
   },
-  plugins: []
+  darkMode: 'class',
+  plugins: [
+    animations,
+    function ({ addVariant }) {
+      addVariant('any-hover', '@media (any-hover: hover) { &:hover }')
+    }
+  ]
 }
